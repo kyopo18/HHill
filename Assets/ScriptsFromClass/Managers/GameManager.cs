952d4446e19e0b1f248b5c0cebdd858053648541
kyopo18 <136762778+kyopo18@users.noreply.github.com>
2024-09-17 08:58:08 -0400

    second to last commit
    few more changes to make game play smoother fixed buggy floor

--- Assets/ScriptsFromClass/Managers/GameManager.cs
+++ Assets/ScriptsFromClass/Managers/GameManager.cs
@@ -39,6 +39,7 @@ public class GameManager : MonoBehaviour
     }
     public void FinishLevel()
     {
+        player = FindObjectOfType<InputController>();
         OnLevelFinish?.Invoke();
     }
 
